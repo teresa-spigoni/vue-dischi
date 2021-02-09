34e69cb8f048b68be69d15425d6f0133c0ec2dd0
Teresa Spigoni <teresaspigoni@gmail.com>
2021-02-09 10:05:28 +0100

    ref

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ new Vue({
     const self = this;
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
     .then(function(the) {
-      for (var i = 0; i < 10; i++) {
+      for (var i = 0; i < the.data.response.length; i++) {
         self.disks.push(the.data.response[i])
         if (!self.genres.includes(the.data.response[i].genre)) {
           self.genres.push(the.data.response[i].genre)
